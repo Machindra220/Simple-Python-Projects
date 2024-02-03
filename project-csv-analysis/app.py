9a3b5bfaa278ad5d30e236bc743061f6b4dc34d7
Machindra220 <machindra220@gmail.com>
2024-02-03 18:30:14 +0530

    working code
    uploaded python code file and Notes in HTML file.

---
+++ project-csv-analysis/app.py
@@ -0,0 +1,49 @@
+import streamlit as st
+import pandas as pd
+#from langchain.chat_models import ChatOpenAI
+from langchain_community.chat_models import ChatOpenAI
+from langchain_experimental.agents import create_pandas_dataframe_agent
+from langchain.agents.agent_types import AgentType
+import tabulate
+
+# Page title
+st.set_page_config(page_title='🦜🔗 ChatGPT for Incident Analysis')
+st.title('🦜🔗 Ask your questions')
+
+# Load CSV file
+def load_csv(input_csv):
+  df = pd.read_csv(input_csv)
+  with st.expander('See DataFrame'):
+    st.write(df)
+  return df
+
+# Generate LLM response
+def generate_response(csv_file, input_query):
+  llm = ChatOpenAI(model_name='gpt-3.5-turbo-0613', temperature=0.2, openai_api_key=openai_api_key)
+  df = load_csv(csv_file)
+  # Create Pandas DataFrame Agent
+  agent = create_pandas_dataframe_agent(llm, df, verbose=True, agent_type=AgentType.OPENAI_FUNCTIONS)
+  # Perform Query using the Agent
+  response = agent.run(input_query)
+  return st.success(response)
+
+# Input widgets
+uploaded_file = st.file_uploader('Upload a CSV file', type=['csv'])
+question_list = [
+  'Other',
+  'How many rows are there?',
+  'List out category wise incidents count?',
+  'How many incidents are related to Database?']
+query_text = st.selectbox('Select an example query:', question_list, disabled=not uploaded_file)
+#openai_api_key = st.text_input('OpenAI API Key', type='password', disabled=not (uploaded_file and query_text))
+openai_api_key = st.sidebar.text_input('OpenAI API Key', type='password', disabled=not (uploaded_file and query_text))
+
+
+# App logic
+if query_text is 'Other':
+  query_text = st.text_input('Enter your query:', placeholder = 'Enter query here ...', disabled=not uploaded_file)
+if not openai_api_key.startswith('sk-'):
+  st.warning('Please enter your OpenAI API key!', icon='⚠')
+if openai_api_key.startswith('sk-') and (uploaded_file is not None):
+  st.header('Output')
+  generate_response(uploaded_file, query_text)
